c9273f52ed2f23db1c1997062c70e9b9fbbd432e
Gottfried Kleinberger <friedi.frieds@gmail.com>
2021-11-18 00:42:57 -0500

    refactor: major overhaul; imported transpose func to DRY up the code

--- wordsearch.js
+++ wordsearch.js
@@ -1,3 +1,5 @@
+const {transpose} = require('/Users/deuxp/Lighthouse/w4/matrix_transposition.js');
+
 /*** WORD SEARCHER **\
  * ======================================================================
  * Input        ->  (letters)   - a 2d array of single string letters
@@ -7,32 +9,21 @@
  * Behaviour    ->  Does not look for words diagonally or backwards
  *                  1. map array method joins the horizontal array into 
  *                     the main list of words (horizontalJoin)
- *                  2. Nested for loops push the the columns to a temporary partition
- *                  3. the column is joined into one string and pushed to 
- *                     the main list of words (horizontalJoin)
+ *                  2. traspose is used to join the columns into words
+ *                  3. horizontal words and vertical words are put into one big list to check
  *                  4. Returns boolean if (word) is included in one of the joined strings
  * ======================================================================
  */
 
 const wordSearch = (letters, word) => {
-  let horizontalJoin = letters.map(ls => ls.join(''));
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  const verticalJoin = transpose(letters).map(ls => ls.join('')); // transpose the 2D array
+  const allWords = horizontalJoin.concat(verticalJoin); // creates a main list of all the horizontal and vertical possibilities 
 
-  for (let i = 0; i < letters[0].length; i++) {
-    ////// i = row
-    let vTemp = []; // temp partition for the column letters to be joined into one word
-    for (let j = 0; j < letters.length; j++) {
-      ///// j = column
-      const column = letters[j][i];
-      vTemp.push(column);
-    }
-    let columnWord = vTemp.join(''); // joins the pushed column letters into one word
-    horizontalJoin = horizontalJoin.concat(columnWord); // adds the joined column word to the list to check against
-  }
-  for (let l of horizontalJoin) {
+  for (let l of allWords) {
     if (l.includes(word)) return true;
   }
   return false;
 };
-
 
 module.exports = wordSearch;
